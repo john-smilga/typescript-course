6206ccaf5055ebea7840a2cb9eff25fcb77e2e25
john smilga <johnsmilga@gmail.com>
2024-02-16 11:07:27 -0500

    add 02-react

--- 02-typescript-react-essentials/src/main.tsx
+++ 02-typescript-react-essentials/src/main.tsx
@@ -7,10 +7,4 @@ import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </Provider>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
